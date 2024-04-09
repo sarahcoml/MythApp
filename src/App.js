df60ce5bd8eeccc1656790418a1226d428d3faa9
Sarah Comlan <119999485+sarahcoml@users.noreply.github.com>
2024-04-08 20:36:18 -0400

    cart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import Navbar from "./components/NavBar";
 import ClothCard from "./components/Card";
 import FilterDropdown from "./components/FilterDropdown";
+import Aggregator from "./components/Aggregator"; // Import the Aggregator component
 import itemsData from "./data/itemsData.json";
 
 function App() {
   const [items, setItems] = useState(itemsData);
   const [filteredItems, setFilteredItems] = useState(items);
+  const [cart, setCart] = useState([]); // Use only one state variable for the cart
 
   const handleSort = (sortBy) => {
     let sortedItems = [...filteredItems];
@@ -34,15 +36,36 @@ function App() {
     setFilteredItems(filtered);
   };
 
+  const addToCart = (item) => {
+    setCart([...cart, item]); // Update the cart state variable when an item is added to the cart
+  };
+
+  // Define function to calculate total
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + parseFloat(item.price.replace('$', '')), 0);
+  };
+
   return (
     <React.Fragment>
       <Navbar />
       <FilterDropdown handleSort={handleSort} handleFilter={handleFilter} />
       <div className="cloth-container">
         {filteredItems.map(item => (
-          <ClothCard key={item.id} item={item} />
+          <ClothCard key={item.id} item={item} addToCart={addToCart} /> // Pass addToCart function to ClothCard
         ))}
       </div>
+      <Aggregator items={cart} /> 
+{/* 
+      <div className="cart-container">
+          <h2>Cart</h2>
+          {cart.length === 0 && <p>The cart is empty.</p>}
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index}>{item.name} - ${item.price}</li>
+            ))}
+          </ul>
+          {cart.length > 0 && <p>Total: ${calculateTotal().toFixed(2)}</p>}
+        </div> */}
     </React.Fragment>
   );
 }
---
+++ src/components/Aggregator.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import itemsData from '../data/itemsData'; 
+
+const Aggregator = ({ items = itemsData }) => {
+    const totalSum = items.reduce((accumulator, currentItem) => accumulator + parseFloat(currentItem.price.replace('$', '')), 0).toFixed(2);
+
+    return (
+        <div className="aggregator-section">
+            <h2>Cart</h2>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <span>{item.title}</span>
+                        <span>{item.price}</span>
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <strong>Total: ${totalSum}</strong>
+            </div>
+        </div>
+    );
+};
+
+export default Aggregator;
